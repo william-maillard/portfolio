3b8ecc396390d07241ec0e8b41ce68cfca3608ea
William MAILLARD <thomasjean228@gmail.com>
2023-08-28 19:44:15 +0200

    Updates

---
+++ precache-manifest.a28c1bd6392cc31355d6ec4842992701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70090bd5a20e94f9fe8776012f38beee",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cf8e7e61c9139f97e6b4",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "0e477300a03db84e5fc1",
+    "url": "/portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "cf8e7e61c9139f97e6b4",
+    "url": "/portfolio/static/js/2.c1874d00.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.c1874d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e477300a03db84e5fc1",
+    "url": "/portfolio/static/js/main.c75adae8.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
