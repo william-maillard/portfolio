5652cc73188005ab80c8067e6ea5a0029ea4ac26
William MAILLARD <thomasjean228@gmail.com>
2023-08-30 11:34:15 +0200

    Updates

--- precache-manifest.9b0fad45fb60c56994d4d3058cb72fc3.js
+++ precache-manifest.9b0fad45fb60c56994d4d3058cb72fc3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cbbd0c5cf4f6ef911ce98186c2ab8f94",
+    "revision": "c0a6b3950fa7546a3ef243ed52bd7d19",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "80b73496946fb7c4268f",
+    "revision": "455ca4ab345b570c559b",
     "url": "/portfolio/static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "2e551661680167f7a6fa",
+    "revision": "c76c95fac87f36754bc1",
     "url": "/portfolio/static/css/main.db76a3b0.chunk.css"
   },
   {
-    "revision": "80b73496946fb7c4268f",
-    "url": "/portfolio/static/js/2.c85cdcdd.chunk.js"
+    "revision": "455ca4ab345b570c559b",
+    "url": "/portfolio/static/js/2.60274504.chunk.js"
   },
   {
     "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
-    "url": "/portfolio/static/js/2.c85cdcdd.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.60274504.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2e551661680167f7a6fa",
-    "url": "/portfolio/static/js/main.1354bfe4.chunk.js"
+    "revision": "c76c95fac87f36754bc1",
+    "url": "/portfolio/static/js/main.45cbca77.chunk.js"
   },
   {
     "revision": "6c972d9418aa5b4e59ab",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a34af50529930961b15e95a66ce358a1.js"
+  "/portfolio/precache-manifest.9b0fad45fb60c56994d4d3058cb72fc3.js"
 );
 
 self.addEventListener('message', (event) => {
