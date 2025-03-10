cd15aa46b6a4932a113db6ae59c6e4d2066ad6cd
William MAILLARD <thomasjean228@gmail.com>
2025-03-10 15:24:34 +0100

    feat: add calendly popup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Typical from "react-typical";
 import Switch from "react-switch";
+import { PopupButton } from "react-calendly";
 
 class Header extends Component {
   titles = [];
@@ -27,20 +28,34 @@ class Header extends Component {
   render() {
     if (this.props.sharedData) {
       var name = this.props.sharedData.name;
-      this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 1500 ] ).flat();
+      this.titles = this.props.sharedData.titles
+        .map((x) => [x.toUpperCase(), 1500])
+        .flat();
     }
 
-    const HeaderTitleTypeAnimation = React.memo( () => {
-      return <Typical className="title-styles" steps={this.titles} loop={50} />
-    }, (props, prevProp) => true);
+    const HeaderTitleTypeAnimation = React.memo(
+      () => {
+        return (
+          <Typical className="title-styles" steps={this.titles} loop={50} />
+        );
+      },
+      (props, prevProp) => true
+    );
 
     return (
-      <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
-        <div className="row aligner" style={{height: '100%'}}>
+      <header
+        id="home"
+        style={{ height: window.innerHeight - 140, display: "block" }}
+      >
+        <div className="row aligner" style={{ height: "100%" }}>
           <div className="col-md-12">
             <div>
-              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
-              <br/>
+              <span
+                className="iconify header-icon"
+                data-icon="la:laptop-code"
+                data-inline="false"
+              ></span>
+              <br />
               <h1 className="mb-0">
                 <Typical steps={[name]} wrapper="p" />
               </h1>
@@ -87,6 +102,17 @@ class Header extends Component {
                 }
                 id="icon-switch"
               />
+              <div id="calendly">
+                <PopupButton
+                  url="https://calendly.com/williammaillard01/premier-contact?hide_gdpr_banner=1"
+                  /*
+                   * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+                   * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+                   */
+                  rootElement={document.getElementById("root")}
+                  text="Click here to schedule!"
+                />
+              </div>
             </div>
           </div>
         </div>
