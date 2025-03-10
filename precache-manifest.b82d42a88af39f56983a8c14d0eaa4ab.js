52dad698aae39bb4abff7965c3f02ee8e9b14286
William <thomasjean228@gmail.com>
2025-03-10 15:57:06 +0000

    Updates

---
+++ precache-manifest.b82d42a88af39f56983a8c14d0eaa4ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf18dcd8814902801942db8d8f907f4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "76615e270fdd99205c3d",
+    "url": "/portfolio/static/css/2.9b44f7c2.chunk.css"
+  },
+  {
+    "revision": "931a1eaa59da79ff7ec5",
+    "url": "/portfolio/static/css/main.0ef3dcc7.chunk.css"
+  },
+  {
+    "revision": "76615e270fdd99205c3d",
+    "url": "/portfolio/static/js/2.68a55319.chunk.js"
+  },
+  {
+    "revision": "d27741c94fb266bcd97997643baacdad",
+    "url": "/portfolio/static/js/2.68a55319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931a1eaa59da79ff7ec5",
+    "url": "/portfolio/static/js/main.5073a661.chunk.js"
+  },
+  {
+    "revision": "58d18144e62526203a32",
+    "url": "/portfolio/static/js/runtime-main.770b1f5e.js"
+  }
+]);
